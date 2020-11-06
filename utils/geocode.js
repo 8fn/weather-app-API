6512524ec6d877d31d5c18213501d2e2acb9f729
tanjilk <tanjilkh@gmail.com>
2020-11-06 23:43:45 +0000

    module export with geocode

--- utils/geocode.js
+++ utils/geocode.js
@@ -12,28 +12,34 @@ const geocode = (address, callback) => {
         url: url,
         json: true,
     }, (error, response) => {
-        if(error){
+        if (error) {
             // Se não conseguir fazer request
             callback('Não deu para conectar aos serviços! Verifique a conexcão da internet.', undefined);
-        } else if(response.body.features.length === 0){
+        } else if (response.body.features.length === 0) {
             callback('O lugar que introduziu não existe. Por favor verifique novamente se o nome do lugar está bem introduzido!', undefined);
         } else {
-            for(let i = 0; i < response.body.features.length; i++){
-                const latitude = response.body.features[i].center[0];
-                const longitude = response.body.features[i].center[1];
-                const place_name = response.body.features[i].place_name;
-                callback(undefined, {
-                    latitude: latitude,
-                    longitude: longitude,
-                    place_name: place_name
-                })
+            if (response.body.features.length > 0) {
+                for (let i = 0; i < response.body.features.length; i++) {
+                    // Coordenadas
+                    const latitude = response.body.features[i].center[0];
+                    const longitude = response.body.features[i].center[1];
+
+                    // Nome do local
+                    const place_name = response.body.features[i].place_name;
+
+                    // Ter as propriedades de cada feature
+                    callback(undefined, {
+                        latitude: latitude,
+                        longitude: longitude,
+                        place_name: place_name
+                    })
+                }
+            } else {
+                callback('Não foi possivel encontrar!', undefined)
             }
         }
     })
 
 }
 
-geocode('porto', (error, data) => {
-    console.log('Error: ', error);
-    console.log('Data: ', data);
-})
+module.exports = geocode;
