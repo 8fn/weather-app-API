205a72f86e191373c5466a82c3211178df99c4a6
tanjilk <tanjilkh@gmail.com>
2020-11-06 23:56:12 +0000

    callback geocode

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Export the geocode inside of the utils
 const geocode = require('./utils/geocode');
 
+// Callback function
 geocode('Porto', (error, data) => {
     console.log('Error: ', error);
     console.log('Data: ', data);
